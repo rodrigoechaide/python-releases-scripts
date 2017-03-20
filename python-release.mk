4b8f468190be06bfa1de66775878fd471b8f9e51
Gonzalo Matheu <gmatheu@ascentio.com.ar>
2017-03-20 15:32:43 -0300

    Making commands work without .bumpversion.cfg

--- python-release.mk
+++ python-release.mk
@@ -24,24 +24,44 @@ __check_defined = \
     $(if $(value $1),, \
       $(error Undefined $1$(if $2, ($2))))
 
+SERIALIZE=--serialize '{major}.{minor}.{patch}-rc{release}' --serialize '{major}.{minor}.{patch}'
+PARSE=--parse '(?P<major>\d+)\.(?P<minor>\d+)\.(?P<patch>\d+)(\-rc(?P<release>[a-z]+))?'
+VERSION=
+UNIT_NAME=
+TAG_NAME=${UNIT_NAME}-{new_version}
+
 # Check that there are no uncommitted changes in the sources.
 check-repo:
-	git diff-index --quiet HEAD -- || echo "There are uncommited chenges in repo"; exit 1;
+	@git diff-index --quiet HEAD -- || echo "There are uncommited changes in repo"; exit 1;
 
 # Change the version to a new version using bumpversion.
-VERSION="patch"
-upgrade-version:
-	bumpversion ${VERSION}
+TAG=--tag
+BUMPVERSION_DEFAULT_ARGS=${SERIALIZE} ${PARSE} --commit ${TAG} --tag-name ${TAG_NAME}
+BUMPVERSION_TEST_ARGS=--verbose --allow-dirty --dry-run
+upgrade-version: check-variables
+	bumpversion ${BUMPVERSION_DEFAULT_ARGS} ${TAG} ${VERSION}
 
-test-upgrade-version:
-	bumpversion ${VERSION} --verbose --allow-dirty --dry-run
+test-upgrade-version: check-variables
+	bumpversion ${BUMPVERSION_DEFAULT_ARGS} ${BUMPVERSION_TEST_ARGS} ${VERSION}
+check-variables:
+	@:$(call check_defined, UNIT_NAME, Unit name as it should be displayed in installers and source packages)
+	@:$(call check_defined, VERSION, which version to release (major, minor, patch))
 
 # Run the tests.
 test:
 	python setup.py test
 
 # Build the code.
-dist: check-repo
+dist:
 	python setup.py sdist
 
-all: check-repo test test-upgrade-version dist
+LAST_TAG=`git tag --sort=-committerdate | head -n 1`
+package-last-tag: check-variables
+	git archive --prefix ${LAST_TAG}/ -o dist/${LAST_TAG}-source.tar.gz ${LAST_TAG}
+
+release: clean test-upgrade-version upgrade-version test dist
+
+clean:
+	rm -rf dist build
+
+.PHONY: dist
